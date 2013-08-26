51209a2b1d39a6525af7d412c08083c53fafa818
Alejandro Santos <alejolp@alejolp.com.ar>
2013-08-26 14:19:00 -0300

    license

--- src/encoder_af1200stj.h
+++ src/encoder_af1200stj.h
@@ -1,8 +1,33 @@
 /*
- * encoder_af1200stj.h
+ * encoder_af1200stj.cpp
  *
  *  Created on: 23/08/2013
  *      Author: alejo
+ *
+ *  Based on the AFSK modem implementation of Sivan Toledo.
+ *
+ *  Code: https://github.com/sivantoledo/javAX25
+ *  QEX paper: http://www.tau.ac.il/~stoledo/Bib/Pubs/QEX-JulAug-2012.pdf
+ *
+ */
+/*
+ * Audio FSK modem for AX25 (1200 Baud, 1200/2200Hz).
+ *
+ * Copyright (C) Sivan Toledo, 2012
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
 #ifndef ENCODER_AF1200STJ_H_
