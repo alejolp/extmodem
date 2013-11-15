56b20ccb41ebde1902a241660a500ab896587f07
Alejandro Santos <alejolp@alejolp.com.ar>
2013-11-15 14:21:08 -0300

    missing ptt files

---
+++ src/ptt_serpar.cpp
@@ -0,0 +1,240 @@
+/*
+ * Sound card modem for Amateur Radio AX25.
+ *
+ * Copyright (C) Alejandro Santos, 2013, alejolp@gmail.com.
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+/*
+ *      Some code was taken from Thomas Sailer's soundmodem:
+ *
+ *      Copyright (C) 1999-2000
+ *        Thomas Sailer (sailer@ife.ee.ethz.ch)
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <exception>
+
+#ifdef __unix__
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#endif
+
+#ifdef __linux
+#include <linux/parport.h>
+#include <linux/ppdev.h>
+#endif
+
+#include "ptt_serpar.h"
+
+namespace extmodem {
+
+
+#ifdef _MSC_VER
+#define IOCTL_SERIAL_SET_DTR            CTL_CODE(FILE_DEVICE_SERIAL_PORT, 9,METHOD_BUFFERED,FILE_ANY_ACCESS)
+#define IOCTL_SERIAL_CLR_DTR            CTL_CODE(FILE_DEVICE_SERIAL_PORT,10,METHOD_BUFFERED,FILE_ANY_ACCESS)
+#define IOCTL_SERIAL_SET_RTS            CTL_CODE(FILE_DEVICE_SERIAL_PORT,12,METHOD_BUFFERED,FILE_ANY_ACCESS)
+#define IOCTL_SERIAL_CLR_RTS            CTL_CODE(FILE_DEVICE_SERIAL_PORT,13,METHOD_BUFFERED,FILE_ANY_ACCESS)
+
+
+ptt_serial_windows::~ptt_serial_windows() {
+	set_tx(0);
+	::CloseHandle(hnd_);
+}
+
+int ptt_serial_windows::init(const char* fname) {
+	BOOLEAN res;
+	DWORD val;
+
+	hnd_ = ::CreateFileA(fname, GENERIC_WRITE, FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+	if (hnd_ == INVALID_HANDLE_VALUE) {
+		std::cerr << "PTT Error: CreateFileA" << std::endl;
+		throw std::exception();
+	}
+
+	res = ::DeviceIoControl(hnd_, IOCTL_SERIAL_CLR_RTS, NULL, 0, NULL, 0, &val, NULL);
+	if (!res) {
+		std::cerr << "PTT Error: DeviceIoControl" << std::endl;
+		::CloseHandle(hnd_);
+		throw std::exception();
+	}
+	return 1;
+}
+
+void ptt_serial_windows::set_tx(int tx) {
+	state_ = !!tx;
+	set_rts(state_);
+	set_dtr(state_);
+}
+
+int ptt_serial_windows::get_tx() {
+	return state_;
+}
+
+void ptt_serial_windows::set_rts(int tx) {
+	DWORD val;
+	DeviceIoControl(hnd_, tx ? IOCTL_SERIAL_SET_RTS : IOCTL_SERIAL_CLR_RTS, NULL, 0, NULL, 0, &val, NULL);
+}
+
+void ptt_serial_windows::set_dtr(int tx) {
+	DWORD val;
+	DeviceIoControl(hnd_, tx ? IOCTL_SERIAL_SET_DTR : IOCTL_SERIAL_CLR_DTR, NULL, 0, NULL, 0, &val, NULL);
+}
+#endif
+
+#ifdef __unix__
+static void unix_print_error(int err) {
+	char buf[1024];
+	strerror_r(err, buf, sizeof(buf));
+	std::cerr << "Error: open() " << buf << std::endl;
+	throw std::exception();
+}
+
+ptt_serial_unix::~ptt_serial_unix() {
+	set_tx(0);
+	if (fd_ > 0)
+		close(fd_);
+}
+
+int ptt_serial_unix::init(const char* fname) {
+	fd_ = open(fname, O_RDWR, 0);
+	if (fd_ < 0) {
+		unix_print_error(errno);
+		return 0;
+	}
+	set_tx(0);
+	return 1;
+}
+
+void ptt_serial_unix::set_tx(int tx) {
+	state_ = !!tx;
+	set_rts(state_);
+	set_dtr(state_);
+}
+
+int ptt_serial_unix::get_tx() {
+	return state_;
+}
+
+void ptt_serial_unix::set_rts(int tx) {
+	unsigned int y;
+	ioctl(fd_, TIOCMGET, &y);
+	if (tx)
+	    y |= TIOCM_RTS;
+	else
+	    y &= ~TIOCM_RTS;
+	ioctl(fd_, TIOCMSET, &y);
+}
+
+void ptt_serial_unix::set_dtr(int tx) {
+	unsigned int y;
+	ioctl(fd_, TIOCMGET, &y);
+	if (tx)
+	    y |= TIOCM_DTR;
+	else
+	    y &= ~TIOCM_DTR;
+	ioctl(fd_, TIOCMSET, &y);
+}
+
+
+ptt_parallel_unix::~ptt_parallel_unix() {
+	set_tx(0);
+	if (fd_ > 0)
+		close(fd_);
+}
+
+int ptt_parallel_unix::init(const char* fname) {
+	fd_ = open(fname, O_RDWR, 0);
+	if (fd_ < 0) {
+		unix_print_error(errno);
+		return 0;
+	}
+
+	if (ioctl(fd_, PPCLAIM, 0)) {
+		unix_print_error(errno);
+		close (fd_);
+		return 0;
+	}
+
+	// Set the Mode
+	int mode = IEEE1284_MODE_BYTE;
+	if (ioctl(fd_, PPSETMODE, &mode)) {
+		unix_print_error(errno);
+		ioctl(fd_, PPRELEASE);
+		close (fd_);
+		return 0;
+	}
+
+	// Set data pins to output
+	int dir = 0x00;
+	if (ioctl(fd_, PPDATADIR, &dir)) {
+		unix_print_error(errno);
+		ioctl(fd_, PPRELEASE);
+		close (fd_);
+		return 1;
+	}
+
+	set_tx(0);
+	return 1;
+}
+
+void ptt_parallel_unix::set_tx(int tx) {
+	state_ = !!tx;
+	unsigned char reg1, reg2;
+
+	if (fd_ < 0)
+		return;
+
+	reg1 = state_ ? 0xff : 0x00;
+	reg2 = 0;
+
+#ifdef __FreeBSD__
+	ioctl(fd_, PPISDATA, &reg1); // High
+	ioctl(fd_, PPISDATA, &reg2); // Low
+#else
+	ioctl(fd_, PPWDATA, &reg1); // High
+	ioctl(fd_, PPWDATA, &reg2); // Low
+#endif
+}
+
+int ptt_parallel_unix::get_tx() {
+	return state_;
+}
+#endif /* __unix__ */
+
+} /* namespace extmodem */
---
+++ src/ptt_serpar.h
@@ -0,0 +1,87 @@
+/*
+ * Sound card modem for Amateur Radio AX25.
+ *
+ * Copyright (C) Alejandro Santos, 2013, alejolp@gmail.com.
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef PTT_SERPAR_H_
+#define PTT_SERPAR_H_
+
+#ifdef _MSC_VER
+#include <windows.h>
+#include <winioctl.h>
+#endif
+
+#include "ptt.h"
+
+
+namespace extmodem {
+
+
+#ifdef _MSC_VER
+class ptt_serial_windows: public ptt {
+public:
+	ptt_serial_windows() : hnd_(INVALID_HANDLE_VALUE) {}
+	virtual ~ptt_serial_windows();
+
+	virtual int init(const char* fname);
+	virtual void set_tx(int tx);
+	virtual int get_tx();
+
+	void set_dtr(int tx);
+	void set_rts(int tx);
+
+private:
+	HANDLE hnd_;
+	int state_;
+};
+#endif
+
+#ifdef __unix__
+class ptt_serial_unix: public ptt {
+public:
+	ptt_serial_unix() : fd_(-1) {}
+	virtual ~ptt_serial_unix();
+
+	virtual int init(const char* fname);
+	virtual void set_tx(int tx);
+	virtual int get_tx();
+
+	void set_dtr(int tx);
+	void set_rts(int tx);
+
+private:
+	int fd_;
+	int state_;
+};
+
+class ptt_parallel_unix: public ptt {
+public:
+	ptt_parallel_unix() : fd_(-1) {}
+	virtual ~ptt_parallel_unix();
+
+	virtual int init(const char* fname);
+	virtual void set_tx(int tx);
+	virtual int get_tx();
+private:
+	int fd_;
+	int state_;
+};
+#endif
+
+} /* namespace extmodem */
+#endif /* PTT_SERPAR_H_ */
