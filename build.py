a083fec4495b901e7c235cda93a5be8afe1836a9
Alejandro Santos <alejolp@gmail.com>
2014-06-30 15:12:33 +0200

    Builder

--- build.py
+++ build.py
@@ -2,46 +2,52 @@
 # -*- coding: utf-8 -*-
 
 # Alejandro Santos LU4EXT alejolp@gmail.com
-# I hate the esoteric syntax of standard builders.
+# I hate the esoteric syntax of standard builders like make.
 
-import os, sys, multiprocessing
+import os, sys, multiprocessing, glob
 
-FILES=['src/audiosource.cpp',
-    'src/audiosource_portaudio.cpp',
-    'src/audiosource_alsa.cpp',
-    'src/decoder_af1200mm.cpp',
-    'src/decoder_af1200stj.cpp',
-    'src/decoder.cpp',
-    'src/decoder_dtmf.cpp',
-    'src/encoder_af1200stj.cpp',
-    'src/encoder.cpp',
-    'src/extconfig.cpp',
-    'src/extexception.cpp',
-    'src/extmodem.cpp',
-    'src/frame.cpp',
-    'src/hdlc.cpp',
-    'src/kiss.cpp',
-    'src/main.cpp',
-    'src/multimon_utils.cpp',
-    'src/ptt.cpp',
-    'src/ptt_serpar.cpp',
-    'src/tcpserver_agwpe.cpp',
-    'src/tcpserver_base.cpp',
-    'src/tcpserver.cpp',
-    'src/tcpserver_kiss.cpp']
+FILES = glob.glob("src/*.cpp")
 
-OBJ_DIR = 'bin'
+OBJ_DIR = 'build'
 CC = 'g++'
-CFLAGS = '-g -O3 -Wall'
-LDFLAGS = '-lportaudio -lboost_system -lboost_program_options'
-EXECUTABLE = 'extmodem'
+CFLAGS = '-MMD -g -O2 -Wall -std=c++11'
+LDFLAGS = ''
+EXECUTABLE = 'dakaraserver'
 JOBS_COUNT = multiprocessing.cpu_count()
 
 # g++ -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/multimon_utils.d" -MT"src/multimon_utils.d" -o "src/multimon_utils.o" "../src/multimon_utils.cpp"
 
 # g++  -o "extmodem"  ./src/audiosource.o ./src/decoder.o ./src/decoder_af1200mm.o ./src/decoder_af1200stj.o ./src/decoder_dtmf.o ./src/extmodem.o ./src/hdlc.o ./src/kiss.o ./src/main.o ./src/multimon_utils.o ./src/tcpserver.o   -lportaudio -lboost_system
 
-def proc_func(p):
+def change_ext(f, ext):
+    return f[:f.rfind('.')] + ext
+
+def deps_func(p):
+    out_bin_path, f = p
+    build = False
+    f_mtime = os.path.getmtime(f)
+    if not os.path.exists(out_bin_path):
+        build = True
+    else:
+        bin_mtime = os.path.getmtime(out_bin_path)
+        if bin_mtime < f_mtime:
+            build = True
+        else:
+            depsfile_path = change_ext(out_bin_path, ".d")
+            if os.path.exists(depsfile_path) and os.path.getmtime(depsfile_path) < f_mtime:
+                with open(depsfile_path) as w:
+                    deps = w.read().replace("\\", " ").replace("\n", " ").split()
+                    assert deps[1][-1] == ':'
+                    del deps[0]
+                for d in deps:
+                    if os.path.getmtime(d) >= bin_mtime:
+                        build = True
+                        break
+    if build:
+        return (out_bin_path, f)
+    return None
+
+def build_func(p):
     out_bin_path, f = p
     cmd = CC + " " + CFLAGS + " -o \"" + out_bin_path + "\" -c \"" + f + "\""
     print(cmd)
@@ -50,23 +56,22 @@ def proc_func(p):
 def main():
     objs = []
     pool = multiprocessing.Pool(JOBS_COUNT)
-    queue = []
 
     if not os.path.exists(OBJ_DIR):
         os.mkdir(OBJ_DIR)
 
     for f in FILES:
-        out_bin_path = os.path.join(OBJ_DIR, \
-            os.path.basename(f[:f.rfind('.')] + ".o"))
-        objs.append(out_bin_path)
-        if not os.path.exists(out_bin_path) or os.path.getmtime(out_bin_path) < os.path.getmtime(f):
-            queue.append((out_bin_path, f))
+        out_bin_path = os.path.join(OBJ_DIR, os.path.basename(change_ext(f, ".o")))
+        objs.append((out_bin_path, f))
 
-    pool.map(proc_func, queue)
+    queue = [x for x in pool.map(deps_func, objs) if x is not None]
 
-    cmd = CC + " -o \"" + os.path.join(OBJ_DIR, EXECUTABLE) + "\" " + ' '.join(objs) + " " + LDFLAGS
-    print(cmd)
-    os.system(cmd)
+    pool.map(build_func, queue)
+
+    if all((os.path.exists(out_bin_path) for out_bin_path, f in objs)):
+        cmd = CC + " -o \"" + os.path.join(OBJ_DIR, EXECUTABLE) + "\" " + ' '.join(objs) + " " + LDFLAGS
+        print(cmd)
+        os.system(cmd)
 
 if __name__ == '__main__':
     main()
